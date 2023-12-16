ec830609f01e745a82c7d4f7fbd80d5cfb58808d
WojD89 <wojciech.dyrka@gmail.com>
2023-12-16 12:15:12 +0100

    feat: js folderr add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Add imports above this line
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector(".gallery");
+
+const createGallery = galleryItems
+  .map(
+    (image) =>
+      `<li>
+            <a class="gallery__item" href="${image.original}">
+              <img
+                class="gallery__image"
+                src="${image.preview}"
+                alt="${image.description}"
+              />
+            </a>
+      </li>`
+  )
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", createGallery);
+
+const gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
+gallery.on("show.simplelightbox");
 console.log(galleryItems);
