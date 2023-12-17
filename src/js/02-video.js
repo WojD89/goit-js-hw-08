c73db0026af46d5faea43647b21d1fe429637c07
WojD89 <wojciech.dyrka@gmail.com>
2023-12-17 10:57:19 +0100

    feat: Error:value to set - fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,7 +7,30 @@ player.on('timeupdate', throttle ((event) => {
     localStorage.setItem('videoplayer-current-time', event.seconds);
 }, 1000));
 
-window.addEventListener('DOMContentLoaded', () => {
-    const time = localStorage.getItem('videoplayer-current-time');
-    player.setCurrentTime(time);
-});
+//window.addEventListener('DOMContentLoaded', () => {
+   // const time = localStorage.getItem('videoplayer-current-time');
+   // player.setCurrentTime(time);
+   // localStorage.clear();
+//});
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+// skopiowane z pliku readMe.
+
+if (storedTime) {
+  player.setCurrentTime(storedTime)
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+}
